72449c532d1587eefbf3e82b59799534fa912171
Dale Bezolli <dalebezolli@gmail.com>
2024-07-10 09:19:17 +0300

    Manage client disconnections

--- server/main.go
+++ server/main.go
@@ -9,6 +9,8 @@ import (
 
 const address = ":8080"
 
+const DEFAULT_ROOM_SIZE = 10
+
 type IPAddress net.Addr
 type RoomID string
 
@@ -21,14 +23,16 @@ const (
 )
 
 type ClientRecord struct {
-	Type   ClientType
-	Name   string
-	Image  string
-	Email  string
-	RoomID RoomID
+	IPAddress IPAddress
+	Type	  ClientType
+	Name	  string
+	Image     string
+	Email     string
+	RoomID    RoomID
 }
 
 type RoomRecord struct {
+	RoomID  RoomID
 	Host    ClientRecord
 	Viewers []ClientRecord
 }
@@ -42,17 +46,18 @@ var activeClients map[IPAddress]ClientRecord
 var activeRooms map[RoomID]RoomRecord
 
 /*
- Caches the client in the activeClients map and sends a response to the client
- If initialization fails, it reutrns false
+	Caches the client in the activeClients map and sends a response to the client
+	If initialization fails, it reutrns false
 */
-func initializeClient(connection *websocket.Conn) bool {
-	_, exists := activeClients[IPAddress(connection.RemoteAddr())]
+func initializeClient(clientIP IPAddress) bool {
+	_, exists := activeClients[clientIP]
 
 	if exists {
 		return false
 	}
 
-	activeClients[IPAddress(connection.RemoteAddr())] = ClientRecord{
+	activeClients[clientIP] = ClientRecord{
+		IPAddress: clientIP,
 		Type: ClientTypeInnactive,
 		Name: "",
 		Image: "",
@@ -60,8 +65,38 @@ func initializeClient(connection *websocket.Conn) bool {
 		RoomID: "",
 	}
 
-	log.Printf("[%s] Initialized client\n", connection.RemoteAddr())
+	log.Printf("[%s] Initialized client\n", clientIP)
 	return true
+}
+
+/*
+	Deletes a client from the ActiveClients Map
+	If he's the host of a room, the room is shutdown as well.
+*/
+func deleteClient(client IPAddress) {
+	clientRecord, clientExists := activeClients[client]
+
+	if clientExists == false {
+		return
+	}
+
+	_, roomExists := activeRooms[clientRecord.RoomID]
+
+	if roomExists && clientRecord.Type == ClientTypeHost {
+		deleteRoom(clientRecord.RoomID);
+	} else if roomExists && clientRecord.Type != ClientTypeHost {
+		// TODO: Remvoe client from room
+	}
+
+	delete(activeClients, client)
+}
+
+/*
+	Deletes a room from the ActiveRooms Map
+	Deletes any user associated with it as well
+*/
+func deleteRoom(roomID RoomID) {
+	// TODO: Cleanup room
 }
 
 func reflect(writer http.ResponseWriter, request *http.Request) {
@@ -78,8 +113,24 @@ func reflect(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	if ok := initializeClient(connection); !ok {
+	if ok := initializeClient(connection.RemoteAddr()); !ok {
 		log.Panicf("[%s] The client is already connected, is this a mistake?\n", connection.RemoteAddr())
+	}
+
+	for {
+		_, data, err := connection.ReadMessage()
+
+		if err != nil {
+			log.Printf("[%s] Potentially disconnected, cleaning up their connection.", connection.RemoteAddr())
+
+			deleteClient(connection.RemoteAddr())
+			connection.Close()
+			return
+		}
+
+		log.Printf("[%s] %s", connection.RemoteAddr(), data)
+
+
 	}
 }
 
