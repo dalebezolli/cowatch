a5ac787982ac3523cc720f604ef0441165f7251b
Dale Bezolli <dalebezolli@gmail.com>
2024-08-07 14:57:53 +0300

    Modify server defaults using flags
    Specifically we added port, innactivity-threshold & cleanup-interval

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 	"os"
 	"time"
@@ -8,10 +9,17 @@ import (
 	"github.com/cowatch/logger"
 )
 
-const address = ":8080"
-const CLIENT_CLEANUP_ROUTINE_INTERVAL = 30
+var port string
+var ClientCleanupRoutineInterval int
+var ClientInnactivityThreshold string
 
 func main() {
+	flag.StringVar(&port, "p", "8080", "Port that the server will run on")
+	flag.StringVar(&ClientInnactivityThreshold, "innactivity-threshold", "600", "The amount of time (sec) a client can be innactive before his session is cleaned up")
+	flag.IntVar(&ClientCleanupRoutineInterval, "cleanup-interval", 30, "The amount of time (sec) the client cleanup will take to rerun")
+
+	flag.Parse()
+
 	logFileName := "log_" + time.Now().Format("2006_01_02_15_04_05.000")
 	logFile, errorOpeningLogFile := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0600)
 	
@@ -24,7 +32,7 @@ func main() {
 		defer logFile.Close()
 	}
 
-	logger.Info("Starting cowatch on %s\n", address)
+	logger.Info("Starting cowatch in port %s\n", port)
 
 	managerInstance := NewManager()
 
@@ -58,12 +66,12 @@ func main() {
 
 	go func() {
 		for true {
-			time.Sleep(CLIENT_CLEANUP_ROUTINE_INTERVAL * time.Second)
+			time.Sleep(time.Duration(ClientCleanupRoutineInterval) * time.Second)
 			managerInstance.CleanupInnactiveClients()
 		}
 	}()
 
-	if err := http.ListenAndServe(address, nil) ; err != nil {
+	if err := http.ListenAndServe(":" + port, nil) ; err != nil {
 		logger.Error("Failed while serving: %s\n", err)
 	}
 }
--- server/manager.go
+++ server/manager.go
@@ -79,8 +79,6 @@ func (manager *Manager) HandleConnection(writer http.ResponseWriter, request *ht
 	}
 }
 
-const CLIENT_INNACTIVITY_THRESHOLD = "90"
-
 func (manager *Manager) CleanupInnactiveClients() {
 	logger.Info("Cleaning up clients\n")
 
@@ -88,7 +86,7 @@ func (manager *Manager) CleanupInnactiveClients() {
 	for _, client := range(manager.clients) {
 		oldNewDifferenceDuration := currentDate.Sub(client.LatestReply)
 		oldNewDifference := time.Time{}.Add(oldNewDifferenceDuration)
-		disconnectThresholdDuration, _ := time.ParseDuration(CLIENT_INNACTIVITY_THRESHOLD + "s")
+		disconnectThresholdDuration, _ := time.ParseDuration(ClientInnactivityThreshold + "s")
 
 		disconnectThreshold := time.Time{}.Add(disconnectThresholdDuration)
 		if oldNewDifference.Compare(disconnectThreshold) == -1 {
