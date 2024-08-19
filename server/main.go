07cb168213d22d6377db190619102067318f56ab
Dale Bezolli <dalebezolli@gmail.com>
2024-08-19 19:03:04 +0300

    Remove alpha test-room

--- server/main.go
+++ server/main.go
@@ -17,7 +17,6 @@ func main() {
 	flag.StringVar(&port, "p", "8080", "Port that the server will run on")
 	flag.StringVar(&ClientInnactivityThreshold, "innactivity-threshold", "600", "The amount of time (sec) a client can be innactive before his session is cleaned up")
 	flag.IntVar(&ClientCleanupRoutineInterval, "cleanup-interval", 30, "The amount of time (sec) the client cleanup will take to rerun")
-
 	flag.Parse()
 
 	logFileName := "log_" + time.Now().Format("2006_01_02_15_04_05.000")
@@ -35,32 +34,6 @@ func main() {
 	logger.Info("Starting cowatch in port %s\n", port)
 
 	managerInstance := NewManager()
-
-	// WARN: The following test data must be removed after (Closed Alpha)
-	managerInstance.clients["private-test"] = &Client{
-		Type:         ClientTypeInnactive,
-		Name:         "Final Boss",
-		Image:        "https://yt3.ggpht.com/yti/ANjgQV9p6GfGGhyml6eA44zBvSER2q3MjEGVcTgSoRFcuJtxvqw=s88-c-k-c0x00ffffff-no-rj",
-		PrivateToken: "private-test",
-		PublicToken:  "public-test",
-	}
-
-	managerInstance.publicToPrivateTokens["public-test"] = "private-test"
-
-	managerInstance.activeRooms["test"] = &Room{
-		RoomID: "test",
-		Host: &Client{
-			Connection: nil,
-			IPAddress:  nil,
-
-			Type:   ClientTypeHost,
-			Name:   "Final Boss",
-			Image:  "https://yt3.ggpht.com/yti/ANjgQV9p6GfGGhyml6eA44zBvSER2q3MjEGVcTgSoRFcuJtxvqw=s88-c-k-c0x00ffffff-no-rj",
-			Email:  "",
-			RoomID: "test",
-		},
-		Viewers: make([]*Client, 0, DEFAULT_ROOM_SIZE),
-	}
 
 	http.HandleFunc("/reflect", managerInstance.HandleConnection)
 
