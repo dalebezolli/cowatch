d96c2eae2e70cd81f8405c13d4d4137d258c767a
Dale Bezolli <dalebezolli@gmail.com>
2025-06-06 22:40:25 +0300

    Setup links

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Fragment, PropsWithChildren } from "react";
+import { Fragment, HTMLAttributes, PropsWithChildren } from "react";
 import Header from "./_components/Header";
 import Image from "next/image";
 
@@ -8,14 +8,14 @@ export default function Home() {
 			<div className="z-50 top-0 w-full fixed bg-dark/50 backdrop-blur-xs">
 				<CenterContainer>
 					<Header links={[
-						{text: "HOME", path: "/"},
-						{text: "FEATURES", path: "/"},
-						{text: "DOWNLOAD", path: "/"},
+						{text: "HOME", path: "#home"},
+						{text: "FEATURES", path: "#features"},
+						{text: "DOWNLOAD", path: "#download"},
 					]} />
 				</CenterContainer>
 			</div>
 
-			<div className="h-[64px]"></div>
+			<div id="#home" className="h-[64px]"></div>
 			<div className="max-md:hidden -z-20 inset-0 fixed pointer-events-none">
 				<div className="absolute left-[20%] w-px h-full bg-white/15"></div>
 				<div className="absolute right-[20%] w-px h-full bg-white/15"></div>
@@ -68,7 +68,7 @@ export default function Home() {
 				</CenterContainer>
 
 
-				<CenterContainer className="mt-[64px] mb-[256px] flex flex-col items-center justify-center gap-[64] md:gap-[192px]">
+				<CenterContainer id="features" className="pt-[64px] pb-[256px] flex flex-col items-center justify-center gap-[64] md:gap-[192px]">
 					<GlassDisplay outerClassName="xl:max-w-[1200px]" className="flex max-xl:flex-col max-md:gap-8 max-xl:gap-16 gap-24 xl:items-center justify-between">
 						<div className="flex flex-col gap-2 md:gap-4 text-left max-w-[390px] max-md:text-sm">
 							<h2 className="text-2xl md:text-4xl font-bold text-light-cyan max-xl:mt-8 mb-4 md:mb-6">Have a party with just one click</h2>
@@ -92,7 +92,7 @@ export default function Home() {
 					</GlassDisplay>
 				</CenterContainer>
 
-				<CenterContainer className="my-[128px] flex flex-col gap-[16px] md:gap-[32px] items-center overflow-hidden max-md:text-sm">
+				<CenterContainer id="download" className="py-[128px] flex flex-col gap-[16px] md:gap-[32px] items-center overflow-hidden max-md:text-sm">
 					<h2 className="text-2xl md:text-4xl font-bold text-light-cyan max-xl:mt-8 mb-6">Want to join in the fun?</h2>
 
 					<div className="relative">
@@ -169,21 +169,19 @@ export default function Home() {
 						<svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M16.3906 2.18095C16.3852 2.17085 16.3763 2.16307 16.3656 2.15908C15.1751 1.61277 13.9188 1.22306 12.6281 0.999701C12.6165 0.997576 12.6045 0.999147 12.5939 1.00419C12.5832 1.00924 12.5744 1.01751 12.5687 1.02783C12.3975 1.33854 12.2421 1.6577 12.1031 1.98408C10.7111 1.77279 9.29514 1.77279 7.90311 1.98408C7.76353 1.65689 7.606 1.33766 7.43124 1.02783C7.42521 1.01781 7.41636 1.00981 7.4058 1.0048C7.39523 0.9998 7.38343 0.998025 7.37186 0.999701C6.07892 1.22206 4.82043 1.61179 3.62811 2.15908C3.61766 2.16351 3.60889 2.17117 3.60311 2.18095C1.22186 5.74033 0.568739 9.20908 0.887489 12.6372C0.888391 12.6458 0.89108 12.6542 0.895384 12.6617C0.899689 12.6692 0.905514 12.6758 0.912489 12.681C2.29957 13.7069 3.85078 14.4899 5.49999 14.9966C5.51184 15.0002 5.52455 15.0001 5.53632 14.9962C5.54809 14.9923 5.55833 14.9847 5.56561 14.9747C5.91947 14.493 6.23303 13.9829 6.50311 13.4497C6.50671 13.4424 6.50875 13.4344 6.50911 13.4262C6.50948 13.418 6.50815 13.4099 6.50521 13.4023C6.50228 13.3946 6.49781 13.3877 6.49207 13.3819C6.48633 13.3761 6.47945 13.3715 6.47186 13.3685C5.9764 13.1786 5.49677 12.9498 5.03749 12.6841C5.02937 12.6791 5.02257 12.6723 5.01765 12.6641C5.01274 12.656 5.00985 12.6468 5.00924 12.6373C5.00863 12.6278 5.0103 12.6183 5.01413 12.6095C5.01796 12.6008 5.02383 12.5932 5.03124 12.5872C5.12811 12.5153 5.22499 12.4403 5.31561 12.3653C5.32361 12.3583 5.33343 12.3537 5.34394 12.352C5.35445 12.3504 5.36522 12.3517 5.37499 12.356C8.38124 13.7278 11.6375 13.7278 14.6094 12.356C14.6188 12.3513 14.6294 12.3494 14.6399 12.3505C14.6504 12.3517 14.6604 12.3557 14.6687 12.3622C14.7594 12.4372 14.8562 12.5153 14.9531 12.5872C14.9605 12.5932 14.9664 12.6008 14.9702 12.6095C14.974 12.6183 14.9757 12.6278 14.9751 12.6373C14.9745 12.6468 14.9716 12.656 14.9667 12.6641C14.9618 12.6723 14.955 12.6791 14.9469 12.6841C14.4888 12.9511 14.0089 13.179 13.5125 13.3653C13.5049 13.3684 13.498 13.3729 13.4923 13.3787C13.4865 13.3846 13.4821 13.3915 13.4791 13.3991C13.4762 13.4068 13.4749 13.4149 13.4752 13.4231C13.4756 13.4312 13.4776 13.4392 13.4812 13.4466C13.756 13.9771 14.0694 14.4868 14.4187 14.9716C14.426 14.9816 14.4363 14.9891 14.448 14.9931C14.4598 14.997 14.4725 14.9971 14.4844 14.9935C16.1383 14.4891 17.694 13.7059 19.0844 12.6778C19.0915 12.6728 19.0974 12.6663 19.1017 12.6587C19.1061 12.6512 19.1087 12.6428 19.1094 12.6341C19.4906 8.6747 18.4656 5.23408 16.3906 2.18095ZM6.95311 10.5497C6.04686 10.5497 5.30311 9.71845 5.30311 8.6997C5.30311 7.68095 6.03436 6.84658 6.95311 6.84658C7.88124 6.84658 8.61874 7.68408 8.60311 8.69658C8.60311 9.71845 7.87186 10.5497 6.95311 10.5497ZM13.0594 10.5497C12.1531 10.5497 11.4094 9.71845 11.4094 8.6997C11.4094 7.68095 12.1375 6.84658 13.0594 6.84658C13.9875 6.84658 14.725 7.68408 14.7094 8.69658C14.7094 9.71845 13.9844 10.5497 13.0594 10.5497Z" fill="#E5F3F3"/>
 						</svg>
-
 					</div>
-
 				</CenterContainer>
 			</main>
 		</Fragment>
 	);
 }
 
-function CenterContainer({ width="1920", className="", children}: PropsWithChildren<{
+function CenterContainer({ width="1920", className="", children, ...rest}: PropsWithChildren<{
 	width?: string;
 	className?: string;
-}>) {
+}> & HTMLAttributes<HTMLDivElement>) {
 	return (
-		<div className={`px-8 lg:px-16 mx-auto ${className}`} style={{ maxWidth: width+"px"}}>
+		<div className={`px-8 lg:px-16 mx-auto ${className}`} style={{ maxWidth: width+"px"}} {...rest}>
 			{children}
 		</div>
 	)
