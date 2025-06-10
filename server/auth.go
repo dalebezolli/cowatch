7589c8711e4cb03e6ff17d2ea6867b1a5f25cf28
Dale Bezolli <dalebezolli@gmail.com>
2025-06-10 20:42:10 +0300

    Track auth errors

--- server/auth.go
+++ server/auth.go
@@ -6,8 +6,10 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/cowatch/logger"
+	"github.com/google/uuid"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
@@ -45,11 +47,15 @@ func (auth *AuthService) handleAuthRoutes() *http.ServeMux {
 }
 
 func (auth *AuthService) routeAuth() http.HandlerFunc {
+	processID, _ := uuid.NewRandom()
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(
 			w,
 			r,
-			auth.authConfig.AuthCodeURL("", oauth2.AccessTypeOffline),
+			auth.authConfig.AuthCodeURL(formatStateString(StateConfig{
+				ProcessId: processID.String(),
+			}), oauth2.AccessTypeOffline),
 			http.StatusTemporaryRedirect,
 		)
 	}
@@ -58,34 +64,53 @@ func (auth *AuthService) routeAuth() http.HandlerFunc {
 func (auth *AuthService) routeAuthCallback() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		code := r.URL.Query().Get("code")
+		state := r.URL.Query().Get("state")
+		stateConfig := parseStateString(state)
+
 		if code == "" {
-			logger.Error("Failed to retrieve code for authenticated user")
-			http.Redirect(w, r, os.Getenv("JOIN_ERROR_REDIRECT"), http.StatusPermanentRedirect)
+			logger.Error("[%s] Failed to retrieve code for authenticated user", stateConfig.ProcessId)
+			http.Redirect(w, r, os.Getenv("JOIN_ERROR_REDIRECT")+"?id="+stateConfig.ProcessId, http.StatusPermanentRedirect)
 			return
 		}
 
 		token, err := auth.authConfig.Exchange(context.Background(), code)
 		if err != nil {
-			logger.Error("Failed to exchange auth code for token")
-			http.Redirect(w, r, os.Getenv("JOIN_ERROR_REDIRECT"), http.StatusPermanentRedirect)
+			logger.Error("[%s] Failed to exchange auth code for token: %s", stateConfig.ProcessId, err.Error())
+			http.Redirect(w, r, os.Getenv("JOIN_ERROR_REDIRECT")+"?id="+stateConfig.ProcessId, http.StatusPermanentRedirect)
 			return
 		}
 
 		client := auth.authConfig.Client(context.Background(), token)
 		response, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
 		if err != nil {
-			logger.Error("Failed to collect user information")
-			http.Redirect(w, r, os.Getenv("JOIN_ERROR_REDIRECT"), http.StatusPermanentRedirect)
+			logger.Error("[%s] Failed to collect user information: %s", stateConfig.ProcessId, err.Error())
+			http.Redirect(w, r, os.Getenv("JOIN_ERROR_REDIRECT")+"?id="+stateConfig.ProcessId, http.StatusPermanentRedirect)
 			return
 		}
 
 		str, err := io.ReadAll(response.Body)
 		if err != nil {
-			http.Redirect(w, r, os.Getenv("JOIN_ERROR_REDIRECT"), http.StatusPermanentRedirect)
+			http.Redirect(w, r, os.Getenv("JOIN_ERROR_REDIRECT")+"?id="+stateConfig.ProcessId, http.StatusPermanentRedirect)
 			return
 		}
 
 		fmt.Println(string(str))
 		http.Redirect(w, r, os.Getenv("JOIN_SUCCESS_REDIRECT"), http.StatusPermanentRedirect)
+	}
+}
+
+type StateConfig struct {
+	ProcessId string
+}
+
+func formatStateString(config StateConfig) string {
+	return "id:" + config.ProcessId
+}
+
+func parseStateString(str string) *StateConfig {
+	id := strings.Trim(str, "id:")
+
+	return &StateConfig{
+		ProcessId: id,
 	}
 }
